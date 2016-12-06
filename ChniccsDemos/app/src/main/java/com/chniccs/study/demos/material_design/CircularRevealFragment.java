c4c9e57a84a23e970d706a48dafbb1ba58d02b74
chencs <chencs@hzanchu.com>
2016-12-06 16:04:53 +0800

    【update】完善CircularReveal效果

--- ChniccsDemos/app/src/main/java/com/chniccs/study/demos/material_design/CircularRevealFragment.java
+++ ChniccsDemos/app/src/main/java/com/chniccs/study/demos/material_design/CircularRevealFragment.java
@@ -1,7 +1,11 @@
 package com.chniccs.study.demos.material_design;
 
+import android.animation.Animator;
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Build;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.Fragment;
 import android.view.View;
 
 import com.chniccs.study.demos.R;
@@ -17,9 +21,10 @@ import butterknife.OnClick;
  * http://www.jianshu.com/p/e30510b83e0e
  * http://blog.csdn.net/tm1989tm/article/details/51025309
  * https://github.com/ozodrukh/CircularReveal
+ * https://github.com/leelei/Android-CircularRevealFragment
  */
 
-public class CircularRevealFragment extends BaseFragment {
+public class CircularRevealFragment extends BaseFragment implements IOnFragmentTouched {
 
     @Override
     public int getLayout() {
@@ -27,16 +32,43 @@ public class CircularRevealFragment extends BaseFragment {
     }
 
     @Override
-    public void initView() {
+    public void initView() {}
+    @Override
+    public void onFragmentTouched(Fragment fragment, float x, float y) {
+        if (fragment instanceof CircularRevealFragment_temp) {
+            final CircularRevealFragment_temp theFragment = (CircularRevealFragment_temp) fragment;
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {//要求5.0以上
+                getActivity().getSupportFragmentManager().beginTransaction().remove(theFragment).commit();
+                return;
+            }
+            Animator unreveal = theFragment.prepareUnrevealAnimator(x, y);
 
-
+            unreveal.addListener(new Animator.AnimatorListener() {
+                @Override
+                public void onAnimationStart(Animator animation) {
+                }
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    //remove the fragment only when the animation finishes
+                    getActivity().getSupportFragmentManager().beginTransaction().remove(theFragment).commit();
+                }
+                @Override
+                public void onAnimationCancel(Animator animation) {
+                }
+                @Override
+                public void onAnimationRepeat(Animator animation) {
+                }
+            });
+            unreveal.start();
+        }
     }
     @OnClick(R.id.fab)
     public void fabClick(View view){
-
-
+        int randomColor = Color.argb(255, (int)(Math.random()*255),(int)(Math.random()*255),(int)(Math.random()*255));
+        Fragment fragment = CircularRevealFragment_temp.newInstance(20,20, randomColor,this);
+        getActivity().getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.container, fragment)
+                .commit();
     }
-
-
-
 }
--- ChniccsDemos/app/src/main/java/com/chniccs/study/demos/material_design/CircularRevealFragment_temp.java
+++ ChniccsDemos/app/src/main/java/com/chniccs/study/demos/material_design/CircularRevealFragment_temp.java
@@ -1,10 +1,17 @@
 package com.chniccs.study.demos.material_design;
 
+import android.animation.Animator;
 import android.annotation.TargetApi;
+import android.app.Activity;
 import android.os.Build;
+import android.os.Bundle;
 import android.transition.Transition;
 import android.transition.TransitionInflater;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewAnimationUtils;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.DecelerateInterpolator;
 
 import com.chniccs.study.demos.R;
 import com.chniccs.study.demos.app.BaseFragment;
@@ -18,78 +25,100 @@ import butterknife.OnClick;
  */
 
 public class CircularRevealFragment_temp extends BaseFragment {
+    private IOnFragmentTouched mListener;
 
     @Override
     public int getLayout() {
-        return 0;
+        return R.layout.fragment_circularreveal_temp;
     }
 
     @Override
     public void initView() {
+        mView.setBackgroundColor(getArguments().getInt("color"));
+
+        // To run the animation as soon as the view is layout in the view hierarchy we add this listener and remove it
+        // as soon as it runs to prevent multiple animations if the view changes bounds
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {//要求5.0以上
-            setupEnterAnimation(); // 入场动画
-//            setupExitAnimation(); // 退场动画
-        } else {
-//            initViews();
+            mView.addOnLayoutChangeListener(new View.OnLayoutChangeListener() {
+                @Override
+                public void onLayoutChange(View v, int left, int top, int right, int bottom, int oldLeft, int oldTop,
+                                           int oldRight, int oldBottom) {
+                    v.removeOnLayoutChangeListener(this);
+                    int cx = getArguments().getInt("cx");
+                    int cy = getArguments().getInt("cy");
+
+                    //get the hypothenuse so the radius is from one corner to the other
+                    int radius = (int) Math.hypot(right, bottom);
+                    Animator reveal = ViewAnimationUtils.createCircularReveal(v, cx, cy, 0, radius);
+                    reveal.setInterpolator(new DecelerateInterpolator(2f));
+                    reveal.setDuration(1000);
+                    reveal.start();
+                }
+            });
         }
-    }
-    @OnClick(R.id.fab)
-    public void fabClick(View view){
 
-    }
-    // 退出按钮
-    public void backActivity(View view) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-//            onBackPressed();
-        } else {
-//            defaultBackPressed();
-        }
-    }
-    // 入场动画
-    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-    private void setupEnterAnimation() {
-        Transition transition = TransitionInflater.from(getActivity())
-                .inflateTransition(R.transition.arc_motion);
-        getActivity().getWindow().setSharedElementEnterTransition(transition);
-        transition.addListener(new Transition.TransitionListener() {
-            @Override public void onTransitionStart(Transition transition) {
-
-            }
-
-            @Override public void onTransitionEnd(Transition transition) {
-                transition.removeListener(this);
-//                animateRevealShow();
-            }
-
-            @Override public void onTransitionCancel(Transition transition) {
-
-            }
-
-            @Override public void onTransitionPause(Transition transition) {
-
-            }
-
-            @Override public void onTransitionResume(Transition transition) {
-
+        mView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (mListener != null){
+                    mListener.onFragmentTouched(CircularRevealFragment_temp.this, event.getX(), event.getY());
+                }
+                return true;
             }
         });
     }
 
-    // 动画展示
-//    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-//    private void animateRevealShow() {
-//        GuiUtils.animateRevealShow(
-//                this, mRlContainer,
-//                mFabCircle.getWidth() / 2, R.color.colorAccent,
-//                new GuiUtils.OnRevealAnimationListener() {
-//                    @Override public void onRevealHide() {
-//
-//                    }
-//
-//                    @Override public void onRevealShow() {
-//                        initViews();
-//                    }
-//                });
-//    }
+    public CircularRevealFragment_temp() {
+    }
 
+    public static CircularRevealFragment_temp newInstance(int centerX, int centerY, int color, IOnFragmentTouched listener) {
+        Bundle args = new Bundle();
+        args.putInt("cx", centerX);
+        args.putInt("cy", centerY);
+        args.putInt("color", color);
+        CircularRevealFragment_temp fragment = new CircularRevealFragment_temp();
+        fragment.setArguments(args);
+        fragment.mListener = listener;
+        return fragment;
+    }
+
+    /**
+     * Get the animator to unreveal the circle
+     *
+     * @param cx center x of the circle (or where the view was touched)
+     * @param cy center y of the circle (or where the view was touched)
+     * @return
+     */
+    public Animator prepareUnrevealAnimator(float cx, float cy) {
+        int radius = getEnclosingCircleRadius(getView(), (int) cx, (int) cy);
+        Animator anim = ViewAnimationUtils.createCircularReveal(getView(), (int) cx, (int) cy, radius, 0);
+        anim.setInterpolator(new AccelerateInterpolator(2f));
+        anim.setDuration(1000);
+        return anim;
+    }
+
+    /**
+     * To be really accurate we have to start the circle on the furthest corner of the view
+     *
+     * @param v  the view to unreveal
+     * @param cx center x of the circle
+     * @param cy center y of the circle
+     * @return the maximum radius
+     */
+    private int getEnclosingCircleRadius(View v, int cx, int cy) {
+        int realCenterX = cx + v.getLeft();
+        int realCenterY = cy + v.getTop();
+        int distanceTopLeft = (int) Math.hypot(realCenterX - v.getLeft(), realCenterY - v.getTop());
+        int distanceTopRight = (int) Math.hypot(v.getRight() - realCenterX, realCenterY - v.getTop());
+        int distanceBottomLeft = (int) Math.hypot(realCenterX - v.getLeft(), v.getBottom() - realCenterY);
+        int distanceBotomRight = (int) Math.hypot(v.getRight() - realCenterX, v.getBottom() - realCenterY);
+
+        return Math.max(Math.max(Math.max(distanceTopLeft, distanceTopRight), distanceBottomLeft), distanceBotomRight);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mListener = null;
+    }
 }
