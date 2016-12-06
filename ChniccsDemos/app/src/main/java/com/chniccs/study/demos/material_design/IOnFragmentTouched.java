c3366cea721da14a04762d002b5590147a8c0e20
chencs <chencs@hzanchu.com>
2016-12-06 17:56:14 +0800

    【add】添加布局文件

---
+++ ChniccsDemos/app/src/main/java/com/chniccs/study/demos/material_design/IOnFragmentTouched.java
@@ -0,0 +1,13 @@
+package com.chniccs.study.demos.material_design;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by ccs on 16/12/6.
+ */
+
+public interface IOnFragmentTouched {
+
+    public void onFragmentTouched(Fragment fragment, float x, float y);
+
+}
